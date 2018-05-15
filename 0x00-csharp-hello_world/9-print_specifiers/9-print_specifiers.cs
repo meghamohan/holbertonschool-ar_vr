371a8e0d4591dbe634667d0f96865d725d63d00a
meghamohan <meghamohan1989@gmail.com>
2018-05-15 00:12:22 -0700

    adding task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine(string.Format("Percent: {0:0.00%}", percent));
+		Console.WriteLine(string.Format("Currency: {0:c}", currency));		
+	}
+}
