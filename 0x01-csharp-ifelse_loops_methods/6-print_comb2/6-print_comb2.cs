47ba339124a138eabc27cbf4556d806735d4a5da
meghamohan <meghamohan1989@gmail.com>
2018-05-16 00:30:14 -0700

    adding task6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				if (i != j)
+				{
+					if ( i == 8 && j == 9 )
+						Console.Write("{0}{1}\n", i, j);
+					else
+						Console.Write("{0}{1}, ", i ,j);
+				}
+			}
+		}	
+        }
+    }
+}
