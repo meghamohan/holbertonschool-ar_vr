036956348e334c3ebbe125bf758c9eef40cbd27c
meghamohan <meghamohan1989@gmail.com>
2018-05-15 23:50:08 -0700

    Adding task5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    for(int i=0; i<100; i++)
+	    {
+	        if(i != 99)
+	            Console.Write("{0}, ",i);
+		else
+		    Console.Write("{0}\n",i);
+	    }
+        }
+    }
+}
