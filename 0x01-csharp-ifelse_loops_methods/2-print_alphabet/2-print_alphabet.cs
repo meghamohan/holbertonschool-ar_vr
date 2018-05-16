20c182b3eb9713e09ea18df04cc3d41ac585cefd
meghamohan <meghamohan1989@gmail.com>
2018-05-15 21:16:41 -0700

    adding task2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a;
+	    for (a = 'a'; a <= 'z'; a++)
+		Console.Write(a);
+        }
+    }
+}
